8c5c21262f88dc1fdb7c42e285703ffb4e44b326
hamdan <hamdan@udel.edu>
2026-09-13 20:12:47 -0400

    Finished state and passed all the tests, check the webage and everything works as supposed to be

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -4,13 +4,47 @@ import { Button } from "react-bootstrap";
 /**
  * Here is a helper function you *must* use to "roll" your die.
  * The function uses the builtin `random` function of the `Math`
- * module (which returns a random decimal between 0 up until 1) in order
- * to produce a random integer between 1 and 6 (inclusive).
+ * module (which returns a random decimal between 0 up until  ) in order
+ * to produce a random integer between   and 6 (inclusive).
  */
 export function d6(): number {
     return 1 + Math.floor(Math.random() * 6);
 }
 
 export function TwoDice(): React.JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftdie, setLeftdie] = useState<number>(3);
+    const [rightdie, setRightdie] = useState<number>(2);
+    return (
+        <div>
+            <span data-testid="left-die">{leftdie} </span>
+            <span data-testid="right-die"> {rightdie}</span>
+
+            <div>
+                <Button
+                    onClick={() => {
+                        setLeftdie(d6());
+                    }}
+                >
+                    Roll Left
+                </Button>
+            </div>
+            <div>
+                <Button
+                    onClick={() => {
+                        setRightdie(d6());
+                    }}
+                >
+                    Roll Right
+                </Button>
+            </div>
+
+            <div>
+                {leftdie === 1 && rightdie === 1 ?
+                    <span>Lose</span>
+                : leftdie === rightdie && leftdie !== 1 && rightdie !== 1 ?
+                    <span> Win</span>
+                :   ""}
+            </div>
+        </div>
+    );
 }
--- src/objects.ts
+++ src/objects.ts
@@ -8,7 +8,6 @@ import { Question, QuestionType } from "./interfaces/question";
 export function makeBlankQuestion(
     id: number,
     name: string,
-<<<<<<< HEAD
     type: QuestionType,
 ): Question {
     return {
@@ -21,11 +20,6 @@ export function makeBlankQuestion(
         points: 1,
         published: false,
     };
-=======
-    type: QuestionType
-): Question {
-    return {};
->>>>>>> upstream/task-state
 }
 
 /**
@@ -36,13 +30,9 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
-<<<<<<< HEAD
     return (
         question.expected.toLowerCase().trim() == answer.toLowerCase().trim()
     );
-=======
-    return false;
->>>>>>> upstream/task-state
 }
 
 /**
@@ -52,7 +42,6 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-<<<<<<< HEAD
     let checker = question.options.some(
         (option: string): boolean => option === answer,
     );
@@ -61,8 +50,6 @@ export function isValid(question: Question, answer: string): boolean {
     } else if (question.type === "multiple_choice_question" && checker) {
         return true;
     }
-=======
->>>>>>> upstream/task-state
     return false;
 }
 
@@ -73,11 +60,7 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-<<<<<<< HEAD
     return question.id + ": " + question.name.slice(0, 10);
-=======
-    return "";
->>>>>>> upstream/task-state
 }
 
 /**
@@ -98,7 +81,6 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-<<<<<<< HEAD
     let body: string = "";
     if (question.type === "multiple_choice_question") {
         body =
@@ -113,9 +95,6 @@ export function toMarkdown(question: Question): string {
     }
     body = "# " + question.name + "\n" + question.body;
     return body;
-=======
-    return "";
->>>>>>> upstream/task-state
 }
 
 /**
@@ -123,11 +102,7 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
-<<<<<<< HEAD
     return { ...question, name: newName };
-=======
-    return question;
->>>>>>> upstream/task-state
 }
 
 /**
@@ -136,12 +111,8 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-<<<<<<< HEAD
     let inverter: boolean = question.published ? false : true;
     return { ...question, published: inverter };
-=======
-    return question;
->>>>>>> upstream/task-state
 }
 
 /**
@@ -151,7 +122,6 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-<<<<<<< HEAD
     return {
         ...oldQuestion,
         id: id,
@@ -159,9 +129,6 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
         options: [...oldQuestion.options],
         published: false,
     };
-=======
-    return oldQuestion;
->>>>>>> upstream/task-state
 }
 
 /**
@@ -172,11 +139,7 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-<<<<<<< HEAD
     return { ...question, options: [...question.options, newOption] };
-=======
-    return question;
->>>>>>> upstream/task-state
 }
 
 /**
@@ -191,7 +154,6 @@ export function mergeQuestion(
     id: number,
     name: string,
     contentQuestion: Question,
-<<<<<<< HEAD
     { points }: { points: number },
 ): Question {
     return {
@@ -202,9 +164,4 @@ export function mergeQuestion(
         options: [...contentQuestion.options],
         published: false,
     };
-=======
-    { points }: { points: number }
-): Question {
-    return contentQuestion;
->>>>>>> upstream/task-state
 }
